397d7eb6c5797d80c3be1b1b48ca366766da8a46
= <=>
2024-05-18 19:20:50 +0300

    first commit

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
      path('',views.products, name = 'products'),
      path('product/<str:name>/', views.product, name='product'),
+     path('addProduct',views.addProduct, name = "addProduct"),
+     path('updateProduct/<int:pid>/',views.updateProduct, name = "updateProduct"),
      
      
 ]
--- products/views.py
+++ products/views.py
@@ -24,3 +24,46 @@ def product(request, name):
 
 
 
+from django.shortcuts import render, redirect
+from .models import Product
+
+
+def addProduct(request):
+    if request.method == 'POST':
+        nameval = request.POST.get('name')
+        contentval = request.POST.get('content')
+        priceval = request.POST.get('price')
+        imageval = request.FILES.get('image')
+        activeval = request.POST.get('active') == 'on'
+        categoryval = request.POST.get('category')
+        
+        
+        
+            
+        new_product = Product(name=nameval,content=contentval, price=priceval, image=imageval,active=activeval,category=categoryval)
+            
+        new_product.save()
+            
+            
+        return redirect('products')
+    
+    return render(request, 'products/addProduct.html')
+
+
+def updateProduct(request, pid):
+    obj = get_object_or_404(Product, id=pid)
+    
+    if request.method == 'POST':
+        obj.name = request.POST.get('name')
+        obj.content = request.POST.get('content')
+        obj.price = request.POST.get('price')
+        if 'image' in request.FILES:
+            obj.image = request.FILES['image']
+        obj.active = 'active' in request.POST
+        obj.category = request.POST.get('category')
+        
+        obj.save()
+        return redirect('products')  
+    
+    context = {'obj': obj}
+    return render(request, 'products/updateProduct.html', context)
